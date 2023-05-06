feeed086a3956b437b13851208dcdb5ebd8b910e
Rohan1366 <rohanghadge97660504@gmail.com>
2023-05-06 12:17:54 +0530

    added

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,7 @@ import CreateProduct from './pages/Admin/CreateProduct';
 import Users from './pages/Admin/Users';
 import Orders from './user/Orders';
 import Profile from './user/Profile';
+import Products from './pages/Admin/Products';
 function App() {
   return (
     <>
@@ -42,6 +43,7 @@ function App() {
         <Route path='admin' element={<AdminDashboard/>} />
         <Route path='admin/create-category' element={<CreateCategory/>} />
         <Route path='admin/create-product' element={<CreateProduct/>} />
+        <Route path='admin/products' element={<Products/>} />
         <Route path='admin/users' element={<Users/>} />
         </Route>
      </Routes>

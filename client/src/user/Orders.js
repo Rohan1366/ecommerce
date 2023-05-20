04b45abbfd17df79cd3b21cdc0b83c66c6f45c26
Rohan1366 <rohanghadge97660504@gmail.com>
2023-05-20 22:18:08 +0530

    added

--- client/src/user/Orders.js
+++ client/src/user/Orders.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import UserMenu from '../components/Layout/UserMenu'
-import Layout from "./../../components/Layout/Layout";
+import Layout from '../components/Layout/Layout'
 import axios from "axios";
-import { useAuth } from "../../context/auth";
+import { useAuth } from "../context/auth";
 import moment from "moment";
 
 const Orders = () => {
@@ -25,7 +25,7 @@ const Orders = () => {
       <div className="container-flui p-3 m-3 dashboard">
         <div className="row">
           <div className="col-md-3">
-            <UserMenu />
+            <UserMenu/>
           </div>
           <div className="col-md-9">
             <h1 className="text-center">All Orders</h1>
